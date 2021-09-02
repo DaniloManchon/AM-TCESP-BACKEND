c64d9913cf2f9a47cb5a458fb1601853bab06dd5
Danilo Manchon dos Santos <danilomanchon@gmail.com>
2021-09-02 19:10:54 -0300

    Update WebSecurityConfig.java

--- src/main/java/com/tcesp/backend/WebSecurityConfig.java
+++ src/main/java/com/tcesp/backend/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.tcesp.backend;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -10,7 +12,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.requiresChannel()
-                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-                .requiresSecure();
+                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+    }
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
     }
 }
